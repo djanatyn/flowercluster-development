335c97c6b8a9cbe05f90360a777b8ba6c85a7ff5
Jonathan Strickland <djanatyn@gmail.com>
2016-09-07 08:24:14 -0700

    run ansible-playbook with sudo

--- roles/ansible/files/update_development.sh
+++ roles/ansible/files/update_development.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 cd /var/local/ansible; sudo -u ansible git pull
-ansible-playbook -i hosts site.yml --vault-password-file=vault_pass "$@"
+sudo ansible-playbook -i hosts site.yml --vault-password-file=vault_pass "$@"
